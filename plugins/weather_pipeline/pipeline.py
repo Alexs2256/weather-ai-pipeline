1587a9921e2192e24754cb9f45dc6148d2be8dec
alexs231 <stavroualex11.@gmail.com>
2026-08-24 15:32:33 -0400

    Fix function names in pipeline runner

--- plugins/weather_pipeline/pipeline.py
+++ plugins/weather_pipeline/pipeline.py
@@ -1,11 +1,12 @@
-from extract import extract_main
-from transform import transform_main
-from load import load_data_to_postgres
+from .extract import extract_from_postgres # or extract_main
+from .transform import transform_weather_with_gemini # or transform_main
+from .load import load_back_to_postgres # or load_main
 
 def run_pipeline():
-    raw_data = extract_main()
-    clean_data = transform_main()
-    load_data_to_postgres(clean_data)
+    # If your scripts are built to run sequentially:
+    raw_data = extract_from_postgres()
+    clean_data = transform_weather_with_gemini(raw_data)
+    load_back_to_postgres(clean_data)
 
 if __name__ == "__main__":
     run_pipeline()
